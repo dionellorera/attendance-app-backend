6552bfedd764ef4be49dc5dc05fd1f0eceb8088c
dione <dione@tagcash.com>
2016-09-29 14:31:28 +0800

    Added error handling for error that returns html

--- public/index.php
+++ public/index.php
@@ -31,7 +31,21 @@ require __DIR__ . '/../src/middleware.php';
 
 // Register routes
 require __DIR__ . '/../src/routes.php'; 
- 
+  
 
-// Run app
+//Error handler, Slim 3 doesnt have this and returns html that makes the app crash
+$c = $app->getContainer();
+$c['errorHandler'] = function ($c) {
+  return function ($request, $response, $exception) use ($c) {
+    $data = [ 
+      'message' => $exception->getMessage()
+    ];
+ 
+    return $c->get('response')->withStatus(500)
+             ->withHeader('Content-Type', 'application/json')
+             ->write(json_encode($data));
+  };
+};
+
+// Run app 
 $app->run();
